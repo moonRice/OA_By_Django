87a807095f6e26f5fe5c616fc8473ee63c543762
moonRice <35357669+moonRice@users.noreply.github.com>
2021-01-19 15:27:31 +0800

    重新优化工程文件结构
    1、将虚拟环境和工程文件分离开。

--- OA_By_Django/asgi.py
+++ OA_By_Django/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for OA project.
+ASGI config for OA_By_Django project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OA.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OA_By_Django.settings')
 
 application = get_asgi_application()
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OA.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OA_By_Django.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
