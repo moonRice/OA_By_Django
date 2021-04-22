422979d5d19b9b4c45dcd6e07864eb02f979fd54
moonRice <35357669+moonRice@users.noreply.github.com>
2021-04-22 10:52:19 +0800

    简易能用的投票系统上线啦！ 完成： 1、这个投票系统能用，能投票还能查看数据； 2、我们提升了全新的log视觉体验，看着更爽哦； 3、小细节优化（GTMAP的LOGO~）； 预期： 4、修复登陆装饰器跳转问题； 5、Tensorflow.js初探索。

--- apps/index/admin.py
+++ apps/index/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+import options.settings as a
 
 # Register your models here.
+# 修改网页title和站点header。
+admin.site.site_title = a.ADMIN_SITE_TITLE_2
+admin.site.site_header = a.ADMIN_SITE_TITLE_1
